c6ea7a5c2162ca11f9acbd48ab0a25187a4dfd76
joshuadimalanta <adrian.dimalanta@obf.ateneo.edu>
2021-10-29 16:07:41 +0800

    interest repository list methods

--- src/app/repository/InterestRepository.java
+++ src/app/repository/InterestRepository.java
@@ -1,5 +1,7 @@
 package app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,11 @@ import app.entity.Interest;
 @Repository
 public interface InterestRepository extends JpaRepository<Interest, Long> {
 
+	public List<Interest> findAllByAgePreference(String agePreference);
+	public List<Interest> findAllBySexPreference(String sexPreference);
+	public List<Interest> findAllByMovieGenre(String movieGenre);
+	public List<Interest> findAllBySongGenre(String songGenre);
+	public List<Interest> findAllByFaveFood(String faveFood);
+	public List<Interest> findAllByFaveHobby(String faveHobby);
+	
 }
