d072acd42b00a0ca9b825c15e01886205c5901fc
reneelastimoso <renee.lastimoso@obf.ateneo.edu>
2021-11-03 15:09:19 +0800

    personal info entity added

--- src/app/entity/PersonalInformation.java
+++ src/app/entity/PersonalInformation.java
@@ -1,5 +1,84 @@
 package app.entity;
 
-public class PersonalInformation {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
+@Entity
+public class PersonalInformation {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	private Long personalInfoID;
+	
+	@Column
+	@NotNull(message = "Name is missing")
+	private String personalInfoName;
+	
+	@Column
+	@NotNull(message = "Age is missing")
+	private Integer age;
+	
+	@Column
+	@NotNull(message = "Pronouns are missing")
+	private String pronouns;
+	
+	@Column
+	@NotNull(message = "Sex is missing")
+	private String sex;
+	
+	//hi
+
+	public Long getPersonalInfoID() {
+		return personalInfoID;
+	}
+
+	public void setPersonalInfoID(Long personalInfoID) {
+		this.personalInfoID = personalInfoID;
+	}
+
+	public String getPersonalInfoName() {
+		return personalInfoName;
+	}
+
+	public void setPersonalInfoName(String personalInfoName) {
+		this.personalInfoName = personalInfoName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getPronouns() {
+		return pronouns;
+	}
+
+	public void setPronouns(String pronouns) {
+		this.pronouns = pronouns;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonalInformation [personalInfoID=" + personalInfoID + ", personalInfoName=" + personalInfoName
+				+ ", age=" + age + ", pronouns=" + pronouns + ", sex=" + sex + "]";
+	}
+
+	
+	
 }
