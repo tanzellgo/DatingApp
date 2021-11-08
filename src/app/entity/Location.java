d9ccd473039889530d68d6b905face70aa5ad252
VR <vrking39@gmail.com>
2021-11-08 20:12:46 +0800

    Added location entitiy contents

--- src/app/entity/Location.java
+++ src/app/entity/Location.java
@@ -1,5 +1,56 @@
 package app.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Location {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	private Long LocationID;
+	
+	@Column
+	@NotNull(message = "Please recheck all the fields inputted.")
+	private String LocationName;
+	
+	@Column
+	@NotNull(message = "Please recheck all the fields inputted.")
+	private String Address;
+
+	@Override
+	public String toString() {
+		return "Location [LocationID=" + LocationID + ", LocationName=" + LocationName + ", Address=" + Address + "]";
+	}
+
+	public Long getLocationID() {
+		return LocationID;
+	}
+
+	public void setLocationID(Long locationID) {
+		LocationID = locationID;
+	}
+
+	public String getLocationName() {
+		return LocationName;
+	}
+
+	public void setLocationName(String locationName) {
+		LocationName = locationName;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+	
+	
 }
