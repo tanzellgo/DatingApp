1bf02653a9ff1486464fb9d305b72445f01070b9
tanzellgo <58267330+tanzellgo@users.noreply.github.com>
2021-11-24 00:48:17 +0800

    Update ContactInformationManager.java

--- src/app/components/ContactInformationManager.java
+++ src/app/components/ContactInformationManager.java
@@ -9,12 +9,42 @@ import app.repository.ContactInformationRepository;
 @Service
 public class ContactInformationManager {
 	
+	String UPLOAD_PATH = "temp/";
+	
 	@Autowired
 	ContactInformationRepository contactInfoRepo;
 	
 	public ContactInformation saveContactInfo(ContactInformation param)
 	{
 		return contactInfoRepo.save(param);
+	}
+	
+	public String saveContactInfoPic(InputStream uploadedInputStream, String filename)
+	{
+        try {
+        	File dir = new File(UPLOAD_PATH);
+        	dir.mkdirs();
+        	
+            int read = 0;
+            byte[] bytes = new byte[1024];
+     
+            File file = new File(dir, filename);
+            
+    		OutputStream out = new FileOutputStream(file);
+            while ((read = uploadedInputStream.read(bytes)) != -1)
+            {
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+			out.close();
+			
+			return "OK - save to image";
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			return "BAD";
+		}
 	}
 
 	public ContactInformation editContactInfo(ContactInformation param) 
